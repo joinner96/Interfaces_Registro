1236b1340dd64b477fcfaf03bdec435eb63cc0f6
zero_ <zero_@Joinner>
2023-07-11 20:36:29 -0500

    Interfaz del usuario creada

---
+++ src/main/java/com/mycompany/interfaces_registro/Interfaces_Registro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfaces_registro;
+
+/**
+ *
+ * @author zero_
+ */
+public class Interfaces_Registro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
